5ce1975a09a7734fa25af4f6efbd20ad6a11765e
Cameron@Netbook <Cameron@Netbook>
2011-08-11 18:17:14 -0400

    Added license stub

--- DeflateStream.hx
+++ DeflateStream.hx
@@ -1,39 +1,46 @@
 /*
-	Copyright (c) 2011, Cameron Desrochers
-	All rights reserved.
+	// TODO: Add BSD-style license
 
-	Redistribution and use in source and binary forms, with or without 
-	modification, are permitted provided that the following conditions are
-	met:
 
-	* Redistributions of source code must retain the above copyright notice, 
-	this list of conditions and the following disclaimer.
+Certain parts of this file are based on the zlib source.
+As such, here is the zlib license in its entirety (from zlib.h):
 
-	* Redistributions in binary form must reproduce the above copyright
-	notice, this list of conditions and the following disclaimer in the 
-	documentation and/or other materials provided with the distribution.
+  zlib.h -- interface of the 'zlib' general purpose compression library
+  version 1.2.5, April 19th, 2010
 
-	* Neither the name of the orignial author nor the names of any 
-	contributors may be used to endorse or promote products derived from 
-	this software without specific prior written permission.
+  Copyright (C) 1995-2010 Jean-loup Gailly and Mark Adler
 
-	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
-	IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-	THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-	PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
-	CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-	EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-	PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-	PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-	LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-	NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+
+  Jean-loup Gailly        Mark Adler
+  jloup@gzip.org          madler@alumni.caltech.edu
+
+
+  The data format used by the zlib library is described by RFCs (Request for
+  Comments) 1950 to 1952 in the files http://www.ietf.org/rfc/rfc1950.txt
+  (zlib format), rfc1951.txt (deflate format) and rfc1952.txt (gzip format).
 */
 
 
+
+
 // References:
-// - The standard zlib implementation (available from http://zlib.net/)
 // - RFC 1950 (zlib) and 1951 (DEFLATE)
+// - The standard zlib implementation (available from http://zlib.net/)
 // - "An Explanation of the Deflate Algorithm" (http://www.zlib.net/feldspar.html)
 // - "Length-Limitted Huffman Codes" (http://cbloomrants.blogspot.com/2010/07/07-02-10-length-limitted-huffman-codes.html)
 // - "Length-Limitted Huffman Codes Heuristic" (http://cbloomrants.blogspot.com/2010/07/07-03-10-length-limitted-huffman-codes.html)
@@ -56,9 +63,9 @@ import haxe.Stack;
 
 enum CompressionLevel {
 	UNCOMPRESSED;		// Fastest
-	FAST;
-	NORMAL;
-	MAXIMUM;
+	FAST;				// Huffman coding only
+	NORMAL;				// Run length encoding
+	MAXIMUM;			// Uses proper LZ77 compression
 }
 
 
