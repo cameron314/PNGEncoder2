f7a52ea78e68d8c0c7e9d470da7b29438b3fe4a0
Cameron@Netbook <Cameron@Netbook>
2011-08-31 23:30:03 -0400

    Added test project (migrated from ImageUploader)

---
+++ PNGEncoder.hx
@@ -0,0 +1,150 @@
+/*
+	Copyright (c) 2008, Adobe Systems Incorporated
+	Copyright (c) 2011, Pimm Hogeling and Edo Rivai
+	All rights reserved.
+
+	Redistribution and use in source and binary forms, with or without 
+	modification, are permitted provided that the following conditions are
+	met:
+
+	* Redistributions of source code must retain the above copyright notice, 
+	this list of conditions and the following disclaimer.
+
+	* Redistributions in binary form must reproduce the above copyright
+	notice, this list of conditions and the following disclaimer in the 
+	documentation and/or other materials provided with the distribution.
+
+	* Neither the name of Adobe Systems Incorporated nor the names of its 
+	contributors may be used to endorse or promote products derived from 
+	this software without specific prior written permission.
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+	IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+	THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+	PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
+	CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+	EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+	PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+	PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+	LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+	NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package;
+import flash.display.Bitmap;
+import flash.display.BitmapData;
+import flash.Lib;
+import flash.utils.ByteArray;
+
+
+/**
+ * Class that converts BitmapData into a valid PNG
+ */	
+class PNGEncoder {
+	/**
+	 * Created a PNG image from the specified BitmapData
+	 *
+	 * @param image The BitmapData that will be converted into the PNG format.
+	 * @return a ByteArray representing the PNG encoded image data.
+	 * @playerversion Flash 9.0
+	 */			
+	public static function encode(img:BitmapData):ByteArray {
+		//var startTime = Lib.getTimer();
+		
+		// Create output byte array
+		var png:ByteArray = new ByteArray();
+		// Write PNG signature
+		png.writeUnsignedInt(0x89504e47);
+		png.writeUnsignedInt(0x0D0A1A0A);
+		// Build IHDR chunk
+		var IHDR:ByteArray = new ByteArray();
+		IHDR.writeInt(img.width);
+		IHDR.writeInt(img.height);
+		IHDR.writeUnsignedInt(0x08060000); // 32bit RGBA
+		IHDR.writeByte(0);
+		writeChunk(png,0x49484452,IHDR);
+		// Build IDAT chunk
+		var IDAT:ByteArray= new ByteArray();
+		//for(var i:int=0;i < img.height;i++) {
+		for(i in 0...img.height) {
+			// no filter
+			IDAT.writeByte(0);
+			var p:UInt;
+			//var j:Int;
+			if ( !img.transparent ) {
+				//for(j=0;j < img.width;j++) {
+				for(j in 0...img.width) {
+					p = img.getPixel(j,i);
+					IDAT.writeUnsignedInt(
+						cast(((p&0xFFFFFF) << 8)|0xFF, UInt));
+				}
+			} else {
+				//for(j=0;j < img.width;j++) {
+				for(j in 0...img.width) {
+					p = img.getPixel32(j,i);
+					IDAT.writeUnsignedInt(
+						cast(((p&0xFFFFFF) << 8)|
+						(p>>>24), UInt));
+				}
+			}
+		}
+		IDAT.compress();
+		writeChunk(png,0x49444154,IDAT);
+		// Build IEND chunk
+		writeChunk(png,0x49454E44,null);
+		// return PNG
+		png.position = 0;
+		//var endTime = Lib.getTimer();
+		//trace("Uncompressed took " + (endTime - startTime) + "ms");
+		
+		return png;
+	}
+
+	private static var crcTable:Array<UInt>;
+	private static var crcTableComputed:Bool  = false;
+
+	private static function writeChunk(png:ByteArray, type:UInt, data:ByteArray):Void {
+		var c:UInt;
+		if (!crcTableComputed) {
+			crcTableComputed = true;
+			crcTable = [];
+			//for (var n:UInt = 0; n < 256; n++) {
+			for (n in 0...256) {
+				c = n;
+				//for (var k:UInt = 0; k < 8; k++) {
+				for (k in 0...8) {
+					if (1 == c & 1) {
+						c = cast(cast(0xedb88320, UInt) ^ 
+							cast(c >>> 1, UInt), UInt);
+					} else {
+						c = cast(c >>> 1, UInt);
+					}
+				}
+				crcTable[n] = c;
+			}
+		}
+		var len:UInt = 0;
+		if (data != null) {
+			len = data.length;
+		}
+		png.writeUnsignedInt(len);
+		var p:UInt = png.position;
+		png.writeUnsignedInt(type);
+		if ( data != null ) {
+			png.writeBytes(data);
+		}
+		var e:UInt = png.position;
+		png.position = p;
+		c = 0xffffffff;
+		//for (var i:Int = 0; i < (e-p); i++) {
+		for (i in 0...(e - p)) {
+			c = cast(crcTable[
+				(c ^ png.readUnsignedByte()) & 
+				cast(0xff, UInt)] ^ cast(c >>> 8, UInt), UInt);
+		}
+		c = cast(c^cast(0xffffffff, UInt), UInt);
+		png.position = e;
+		png.writeUnsignedInt(c);
+	}
+}
---
+++ Test.hx
@@ -0,0 +1,185 @@
+package;
+import flash.Boot;
+import flash.display.Bitmap;
+import flash.display.BitmapData;
+import flash.display.Loader;
+import flash.display.Sprite;
+import flash.events.Event;
+import flash.events.IOErrorEvent;
+import flash.events.MouseEvent;
+import flash.Lib;
+import flash.net.FileReference;
+import flash.text.TextField;
+import flash.text.TextFieldAutoSize;
+import flash.utils.ByteArray;
+import flash.utils.Timer;
+import PNGEncoder2;
+import DeflateStream;
+
+class Test extends Sprite
+{
+	private static inline var MARGIN = 10;
+	
+	
+	public static function main()
+	{
+		Lib.current.addChild(new Test(200, 200));
+	}
+	
+	
+	public function new(?width : Int, ?height : Int)
+	{
+		super();
+		
+		DeflateStreamTests.run();
+		trace("Tests passed\n");
+		
+		if (width == null) width = 1024;
+		if (height == null) height = 2048;
+		
+		var bmp = new BitmapData(width, height, true, 0x00FFFFFF);
+		bmp.perlinNoise(width, height, 2, 0xDEADBEEF, false, false);
+		
+		var display = new Bitmap(bmp);
+		display.x = MARGIN;
+		display.y = 250;
+		addChild(display);
+		
+		//doBenchmark(bmp);
+		
+		var that = this;
+		PNGEncoder2.level = CompressionLevel.NORMAL;
+		var encoder = PNGEncoder2.encodeAsync(bmp);
+		encoder.addEventListener(Event.COMPLETE, function (e) {
+			var loader = new Loader();
+			loader.loadBytes(encoder.png);
+			that.addChild(loader);
+			
+			that.doubleClickEnabled = true;
+			that.addEventListener(MouseEvent.DOUBLE_CLICK, function (e2) {
+				var fileReference = new FileReference();
+				fileReference.save(encoder.png, "image.png");
+			});
+		});
+		
+		
+		/*var png = PNGEncoder2.encode(bmp);
+		var loader = new Loader();
+		loader.loadBytes(png);
+		addChild(loader);
+		doubleClickEnabled = true;
+		addEventListener(MouseEvent.DOUBLE_CLICK, function (e2) {
+			var fileReference = new FileReference();
+			fileReference.save(png, "image.png");
+		});*/
+	}
+	
+	
+	private function doBenchmark(bmp)
+	{
+		// Warm up
+		var data1 = PNGEncoder.encode(bmp);
+		var data2 = PNGEncoder2.encode(bmp);
+		
+		var loader = new Loader();
+		loader.addEventListener(IOErrorEvent.IO_ERROR, function (e) {
+			trace("Error reading PNG that was compressed with optimized encoder\n");
+		});
+		
+		doubleClickEnabled = true;
+		addEventListener(MouseEvent.DOUBLE_CLICK, function (e) {
+			var fileReference = new FileReference();
+			fileReference.save(data2, "test_png.png");
+		});
+		
+		loader.loadBytes(data2);
+		loader.x = MARGIN + bmp.width + 10;
+		loader.y = 250;
+		addChild(loader);
+		
+		
+		
+		trace("Encoders yield same bytes: " + compare(data1, data2) + "\n");
+		trace("PNGEncoder byte count:\t\t\t" + data1.length);
+		trace("PNGEncoder2 byte count:\t\t\t" + data2.length);
+		trace("% better:\t\t\t\t\t\t\t\t" + round((1 - 1.0 * data2.length / data1.length) * 100, 1) + "%\n");
+		
+		var runs = 5;
+		var pngTime = testPNGEncoder(bmp, runs);
+		var opPngTime = testOptimizedPNGEncoder(bmp, runs);
+		
+		trace("PNGEncoder:\t\t\t" + pngTime + "ms");
+		trace("PNGEncoder2:\t\t\t" + opPngTime + "ms");
+		trace("x better:\t\t\t\t\t" + round(1.0 * pngTime / opPngTime, 2) + "x\n");
+	}
+	
+	
+	private static function testPNGEncoder(bmp : BitmapData, runs : Int) : Int
+	{
+		return time(callback(PNGEncoder.encode, bmp), runs);
+	}
+	
+	private static function round(num : Float, decimalPlaces : Int) : Float
+	{
+		var multiplier = Math.pow(10, decimalPlaces);
+		return Math.round(num * multiplier) / multiplier;
+	}
+	
+	private static function compare(a : ByteArray, b : ByteArray) : Bool
+	{
+		if (a == b) {
+			return true;
+		}
+		
+		if (a == null || b == null) {
+			return false;
+		}
+		
+		if (a.length != b.length) {
+			return false;
+		}
+		
+		for (i in 0...a.length) {
+			if (a.readUnsignedByte() != b.readUnsignedByte()) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	
+	private static function testOptimizedPNGEncoder(bmp : BitmapData, runs : Int) : Int
+	{
+		return time(callback(PNGEncoder2.encode, bmp), runs);
+	}
+	
+	
+	private static function time(func : Void -> Dynamic, runs : Int) : Int
+	{
+		var times = [  ];
+		
+		for (i in 0...runs) {
+			var start = Lib.getTimer();
+			func();
+			var stop = Lib.getTimer();
+			times.push(stop - start);
+		}
+		
+		if (runs == 1) {
+			return times[0];
+		}
+		
+		// Sum times, and find the largest time (will be excluded)
+		var largest = 0;
+		var total = 0;
+		for (time in times) {
+			total += time;
+			if (time > largest) {
+				largest = time;
+			}
+		}
+		
+		return Std.int((total - largest) / (runs - 1));
+	}
+}
